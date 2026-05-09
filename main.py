761e953ddacdc271814370af910c785a6adb0507
Myles Gaerlan <ish.myles@gmail.com>
2026-05-09 22:15:33 +1000

    feat: add fps to game loop
    Added fps to the game loop to restrict game to draw a max of 60FPS

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,6 +20,9 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        delta_time = clock.tick(60)
+        dt = delta_time / 1000
+
         
 
 if __name__ == "__main__":
